d3c6469a93ebc869439c061d8c112e4caebdb5de
Alina-Sapunova <alina090207@mail.ru>
2024-05-02 14:12:03 +0300

    telebot команда некст и 2вопрос

--- main.py
+++ main.py
@@ -37,25 +37,40 @@ def go_comand(message):
 def check_one(call):
     if call.data == 'button2':
         file = open('C:\Kosmos\img\gagarin.jpg', 'rb')
-        bot.send_photo(call.message.chat.id, file, 'Верно! Ю.А. Гагарин был первым кто полетел в космос.')
+        bot.send_photo(call.message.chat.id, file, 'Верно! Ю.А. Гагарин был первым кто полетел в космос. '
+                                                   'Нажми на команду /next для следующего вопроса')
     elif call.data == 'button':
         bot.send_message(call.message.chat.id, 'Неверно:( С.П. Королев — выдающийся конструктор и учёный, '
                                                'работавший в области ракетной и ракетно-космической техники.Попробуй ёщё!')
-    else:
+    elif call.data == 'button3':
         bot.send_message(call.message.chat.id, 'Неверно:( Г.С. Титов — второй советский космонавт, '
                                                'второй человек в мире, совершивший орбитальный космический полёт.'
                                                'Попробуй ёщё!')
+    elif call.data == 'btn2' or call.data == 'btn3':
+        bot.send_message(call.message.chat.id, 'Неверно:( Попробуй ёщё!')
+    elif call.data == 'btn':
+        bot.send_message(call.message.chat.id, 'Верно! 12 апреля-День космонавтики.')
+
+
+@bot.message_handler(commands=['next'])
+def next_comand(message):
+    klava = types.InlineKeyboardMarkup(row_width=1)
+    button = types.InlineKeyboardButton(text='12 апреля', callback_data='btn')
+    button2 = types.InlineKeyboardButton(text='7 апреля', callback_data='btn2')
+    button3 = types.InlineKeyboardButton(text='1 мая', callback_data='btn3')
+    klava.add(button, button2, button3)
+    bot.send_message(message.chat.id, 'День космонавтики?', reply_markup=klava)
 
 
 @bot.message_handler(commands=['inline'])
 def inline_comand(message):
     klava = types.InlineKeyboardMarkup(row_width=1)
-    button = types.InlineKeyboardButton(text='Полезные ссылки',
+    button = types.InlineKeyboardButton(text='Первая женщина космонавт',
                                         url='https://ru.wikipedia.org/wiki/%D0%A2%D0%B5%D1%80%D0%B5%D1%88%D0%BA%D0%BE%D0%B2%D0%B0,_%D0%92%D0%B0%D0%BB%D0%B5%D0%BD%D1%82%D0%B8%D0%BD%D0%B0_%D0%92%D0%BB%D0%B0%D0%B4%D0%B8%D0%BC%D0%B8%D1%80%D0%BE%D0%B2%D0%BD%D0%B0')
     button2 = types.InlineKeyboardButton(text='Ю.А. Гагарин',
                                          url='https://ru.wikipedia.org/wiki/%D0%93%D0%B0%D0%B3%D0%B0%D1%80%D0%B8%D0%BD,_%D0%AE%D1%80%D0%B8%D0%B9_%D0%90%D0%BB%D0%B5%D0%BA%D1%81%D0%B5%D0%B5%D0%B2%D0%B8%D1%87')
     klava.add(button, button2)
-    bot.send_message(message.chat.id, 'Первый космонавт Земли?', reply_markup=klava)
+    bot.send_message(message.chat.id, 'Полезные ссылки', reply_markup=klava)
 
 
 @bot.message_handler(commands=['help'])
